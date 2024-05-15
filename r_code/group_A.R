ccea34b855893bf11121111bd3457d3b4391d29f
ievarau <ievarau@uio.no>
2024-05-15 14:41:04 +0200

    group A code finished maybe

--- r_code/group_A.R
+++ r_code/group_A.R
@@ -15,16 +15,21 @@ dir.create(OUTPUT_DIR, recursive = TRUE, showWarnings = FALSE)
 DATA_DIR <- file.path(OUTPUT_DIR, "data")
 dir.create(DATA_DIR, recursive = TRUE, showWarnings = FALSE)
 
+## Produced results:
+RESULTS_DIR <- file.path(OUTPUT_DIR, "results")
+dir.create(RESULTS_DIR, recursive = TRUE, showWarnings = FALSE)
+
 #########################
 ## Simulating the data ##
 #########################
 
+COUNT_MAT_PATH <- file.path(DATA_DIR, "count_matrix.RData")
+METADATA_PATH  <- file.path(DATA_DIR, "metadata.tsv")
+
 ## DATA 1
 ## Create example RNA-seq count matrix and export it
 
-COUNT_MAT_PATH <- file.path(DATA_DIR, "count_matrix.RData")
-
-set.seed(123)
+set.seed(567)
 num_genes <- 1000
 num_samples <- 10
 
@@ -38,53 +43,108 @@ save(
 
 ## DATA 2
 ## Create metadata and export it
+
 metadata <- data.frame(
     Sample = colnames(count_matrix),
     Condition = sample(c("Control", "Treatment"), size = num_samples, replace = TRUE),
     stringsAsFactors = FALSE)
 
-
+data.table::fwrite(
+    metadata,
+    file = METADATA_PATH,
+    sep = "\t",
+    col.names = TRUE)
 
 ####################################
 ## Reading and checking the input ##
 ####################################
 
-# Step 2: Read the exported matrix file and check for numerical values and missing values
-count_matrix <- read.csv("count_matrix.csv", header = TRUE, row.names = 1)
-if (!all(sapply(count_matrix, is.numeric)) || any(is.na(count_matrix))) {
-  stop("Matrix contains non-numeric values or missing values")
+## Loading the count matrix:
+count_matrix <- get(load(COUNT_MAT_PATH))
+
+## Reading the metadata:
+metadata <- data.table::fread(
+    METADATA_PATH,
+    header = TRUE,
+    sep = "\t")
+
+## Checking the input:
+if (!all(sapply(count_matrix, is.numeric))) {
+    stop("Matrix contains non-numeric values.")
 }
 
+if (any(is.na(count_matrix))) {
+    warning("Matrix contains missing values. Consider adding pseudocounts or data imputation.")
+}
 
+##########################
+## Processing the input ##
+##########################
 
-# Step 3: Impute missing values using average
+## Impute missing values using average:
 count_matrix[is.na(count_matrix)] <- rowMeans(count_matrix, na.rm = TRUE)
 
+## Add pseudocount:
+pseudocount <- 1
+count_matrix <- count_matrix + pseudocount
 
+## Filtering lowly expressed genes:
+min_counts <- 10
+keep_genes <- rowSums(count_matrix >= min_counts) > 0
+count_matrix <- count_matrix[keep_genes, ]
 
+#######################################################
+## Performming differential gene expression analysis ##
+#######################################################
 
-# Step 4: Perform differential gene expression using DESeq2
-dds <- DESeqDataSetFromMatrix(countData = count_matrix,
-                              colData = metadata,
-                              design = ~ Condition)
+## Perform differential gene expression using DESeq2
+dds <- DESeqDataSetFromMatrix(
+    countData = count_matrix,
+    colData = metadata,
+    design = ~ Condition)
 dds <- DESeq(dds)
 
-
-## Pre-filter lowly expressed genes:
-smallestGroupSize <- 3
-keep <- rowSums(counts(dds) >= 10) >= smallestGroupSize
-dds <- dds[keep,]
-
-
-
-# Extract differential expression results
+## Extract differential expression results
 res <- results(dds)
 
-# Step 5: Plot differential gene expression results using ggplot2
-# For example, let's plot a volcano plot
-volcano_plot <- ggplot(data = res, aes(x = log2FoldChange, y = -log10(pvalue))) +
-  geom_point(size = 1.5, color = ifelse(res$padj < 0.05, "red", "black")) +
-  labs(x = "Log2 Fold Change", y = "-Log10 p-value", title = "Volcano Plot") +
-  theme_minimal()
+###################################################
+## Plotting differential gene expression results ##
+###################################################
 
-print(volcano_plot)
+results_for_plotting <- as.data.frame(res)
+
+volcano_plot <- ggplot(
+        data = results_for_plotting,
+        aes(
+            x = log2FoldChange,
+            y = -log10(pvalue))) +
+    geom_point(
+        size = 1.5,
+        color = ifelse(res$padj < 0.05, "red", "black")) +
+    labs(
+        x = "Log2 Fold Change",
+        y = "-Log10 p-value",
+        title = "DGE results") +
+    theme_minimal()
+
+###########################
+## Exporting the results ##
+###########################
+
+PATH_TO_DGE_RES <- file.path(RESULTS_DIR, "DGE_results.tsv")
+PATH_TO_DGE_PLOT <- file.path(RESULTS_DIR, "DGE_results.pdf")
+
+data.table::fwrite(
+    results_for_plotting,
+    file = PATH_TO_DGE_RES,
+    sep = "\t",
+    col.names = TRUE)
+
+ggsave(
+    volcano_plot,
+    file = PATH_TO_DGE_PLOT,
+    height = 6,
+    width  = 6,
+    limitsize = FALSE)
+
+
