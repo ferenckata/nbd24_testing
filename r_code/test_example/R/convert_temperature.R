ef4b7fd101854b54b5b9d1df881a29d2f12a4836
ievarau <ievarau@uio.no>
2024-05-16 14:21:42 +0200

    Added test examples

---
+++ r_code/test_example/R/convert_temperature.R
@@ -0,0 +1,21 @@
+#' Converting temperature in Farenheit to Celsius
+#' 
+#' @param F_temp Temperature in Farenheit
+#' @returns Temperature in Celsius
+#' @examples
+#' F_to_C(10)
+F_to_C <- function(F_temp){
+    C_temp <- (F_temp - 32) * 5/9;
+    return(C_temp);
+}
+
+#' Converting temperature in Celsius to Farenheit
+#' 
+#' @param C_temp
+#' @returns Temperature in Farenheit
+#' @examples
+#' C_to_F(10)
+C_to_F <- function(C_temp){
+    F_temp <- (C_temp * 9/5) + 32;
+    return(F_temp);
+}
