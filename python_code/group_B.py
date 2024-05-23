84bf3e4d5e6d75e4b87a1d2a63b75d4f68aa3927
Ladislav Hovan <19410951+ladislav-hovan@users.noreply.github.com>
2024-05-23 15:50:11 +0200

    Add code with functions for group B

---
+++ python_code/group_B.py
@@ -0,0 +1,92 @@
+###########################
+## Load required modules ##
+###########################
+
+import os
+
+import pandas as pd
+
+from pathlib import Path
+
+############################
+## Sourcing the functions ##
+############################
+
+from group_B_functions import *
+
+#################################
+## Defining output directories ##
+#################################
+
+## Main output directory:
+OUTPUT_DIR = Path('results')
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+## Generated data:
+DATA_DIR = os.path.join(OUTPUT_DIR, 'data')
+os.makedirs(DATA_DIR, exist_ok=True)
+
+## Produced results:
+RESULTS_DIR = os.path.join(OUTPUT_DIR, 'results')
+os.makedirs(RESULTS_DIR, exist_ok=True)
+
+#########################
+## Simulating the data ##
+#########################
+
+COUNT_MAT_PATH = os.path.join(DATA_DIR, 'count_matrix.pkl')
+METADATA_PATH = os.path.join(DATA_DIR, 'metadata.tsv')
+
+## Create example RNA-seq count matrix and metadata
+count_df,metadata = generate_counts_with_metadata(
+    COUNT_MAT_PATH,
+    METADATA_PATH,
+    number_of_genes=1000,
+    number_of_samples=10,
+    condition_vector=['Control', 'Treatment'],
+    seed=567,
+)
+
+####################################
+## Reading and checking the input ##
+####################################
+
+## Loading the count matrix:
+count_df = pd.read_pickle(COUNT_MAT_PATH)
+
+## Reading the metadata:
+metadata = pd.read_csv(METADATA_PATH, sep='\t', index_col=0)
+
+## Checking the input:
+check_counts(count_df)
+    
+##########################
+## Processing the input ##
+##########################
+
+## Impute missing values using average:
+count_df = process_counts(count_df, pseudocount=1, min_counts=10)
+
+#######################################################
+## Performming differential gene expression analysis ##
+#######################################################
+
+## Perform differential gene expression using PyDESeq2
+res = calculate_deg(count_df, metadata, design_factors='Condition')
+
+###################################################
+## Plotting differential gene expression results ##
+###################################################
+
+ax = plot_deg_results(res)
+
+###########################
+## Exporting the results ##
+###########################
+
+PATH_TO_DGE_RES = os.path.join(RESULTS_DIR, 'DGE_results.tsv')
+PATH_TO_DGE_PLOT = os.path.join(RESULTS_DIR, 'DGE_results.pdf')
+
+res.to_csv(PATH_TO_DGE_RES, sep='\t')
+
+ax.get_figure().savefig(PATH_TO_DGE_PLOT, dpi=300)
